e0eb20baf87c81633583f7d97ff5d61bfeb19abc
Roger Jaggi <roger.jaggi@gmail.com>
2014-02-28 14:40:15 +0100

    Created PeopleTag app

---
+++ gen/ch/bfh/fbi/mobiComp/PeopleTag/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package ch.bfh.fbi.mobiComp.PeopleTag;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
