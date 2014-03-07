b38887c9ec8c3e6d38af4f8c74c36e5f38b46b39
bohnp1 <bohnp1@bfh.ch>
2014-03-07 15:42:35 +0100

    add menu for user interaction

--- src/ch/bfh/fbi/mobiComp/PeopleTag/MainActivity.java
+++ src/ch/bfh/fbi/mobiComp/PeopleTag/MainActivity.java
@@ -2,6 +2,11 @@ package ch.bfh.fbi.mobiComp.PeopleTag;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +16,46 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
     }
+
+
+    public void registerPosition(View view)
+    {
+        Toast.makeText(this, "Position Registered", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuitem_search:
+                Toast.makeText(this, getString(R.string.ui_menu_search),
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menuitem_send:
+                Toast.makeText(this, getString(R.string.ui_menu_send),
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menuitem_add:
+                Toast.makeText(this, getString(R.string.ui_menu_add),
+                        Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.menuitem_quit:
+                Toast.makeText(this, getString(R.string.ui_menu_quit),
+                        Toast.LENGTH_SHORT).show();
+                finish(); // close the activity
+                return true;
+        }
+        return false;
+    }
+
 }
