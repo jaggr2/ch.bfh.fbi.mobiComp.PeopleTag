2499140b2336455ceee28b2725199c9e914382ae
Roger Jaggi <roger.jaggi@blastbeatsyndicate.com>
2014-03-28 18:48:30 +0100

    Improved SetupActivity

--- AndroidApp/src/ch/bfh/fbi/mobiComp/PeopleTag/gui/SetupActivity.java
+++ AndroidApp/src/ch/bfh/fbi/mobiComp/PeopleTag/gui/SetupActivity.java
@@ -5,16 +5,34 @@ import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 import ch.bfh.fbi.mobiComp.PeopleTag.R;
-import ch.bfh.fbi.mobiComp.PeopleTag.tasks.UserRegisterTask;
+import ch.bfh.fbi.mobiComp.PeopleTag.model.UserData;
+import ch.bfh.fbi.mobiComp.PeopleTag.tasks.UserUpdateTask;
 
 public class SetupActivity extends Activity {
 
+    private static SetupActivity instance;
+
+    public static SetupActivity getInstance() {
+        if(instance == null) {
+            instance = new SetupActivity();
+        }
+        return instance;
+    }
+
     public static final String PREFS_NAME = "PeopleTagUser";
+    public static final String PREF_ID = "ID";
+    public static final String PREF_DISPLAYNAME = "displayName";
+    private SharedPreferences settings = null;
+
+    public String getUserID() {
+        return (settings != null ? this.settings.getString(PREF_ID, "") : "");
+    }
+
+    public String getDisplayName() {
+        return (settings != null ? this.settings.getString(PREF_DISPLAYNAME, "") : "");
+    }
 
     /**
      * Called when the activity is first created.
@@ -23,40 +41,55 @@ public class SetupActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setup);
 
+        settings = getSharedPreferences(PREFS_NAME, 0);
+
         final Button button = (Button) findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 registerUser(v);
             }
         });
+        button.setText((getUserID().length() > 0 ? "Update" : "Register"));
 
-        // Restore preferences
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        String id = settings.getString("ID", "");
+        final ProgressBar progressBarSetup = (ProgressBar) findViewById(R.id.progressBarSetup);
+        progressBarSetup.setVisibility(View.INVISIBLE);
 
-        if(id != null && id.length() > 0) {
-            TextView textViewTitle = (TextView) findViewById(R.id.textViewTitle);
-            textViewTitle.setText("ID" + id);
-        }
+        final EditText editText = (EditText)this.findViewById(R.id.editTextDisplayName);
+        editText.setText(getDisplayName());
 
-        TextView viewLatitude = (TextView) findViewById(R.id.textViewLatitudeValue);
-        TextView viewLongitude = (TextView) findViewById(R.id.textViewLongitudeValue);
+        final TextView textViewTitle = (TextView) findViewById(R.id.textViewTitle);
+        textViewTitle.setText((getUserID().length() > 0 ? "Update your user" : "Register as new User"));
+
+        final TextView textViewSubTitle = (TextView) findViewById(R.id.textViewSubTitle);
+        textViewSubTitle.setText((getUserID().length() > 0 ? "Your ID: " + getUserID() : "Your ID: none"));
+        textViewSubTitle.setVisibility((getUserID().length() > 0 ? View.VISIBLE : View.INVISIBLE));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
         MainActivity mainActivity = MainActivity.getInstance();
-
         Location loc = (mainActivity.getActualLocation() != null ? mainActivity.getActualLocation() : null);
 
-        if (loc != null)
-        {
-            viewLongitude.setText(Double.toString(loc.getLongitude()));
-            viewLatitude.setText(Double.toString(loc.getLatitude()));
+        if(loc != null) {
+            final TextView textViewLongitudeValue = (TextView) findViewById(R.id.textViewLongitudeValue);
+            textViewLongitudeValue.setText(String.valueOf(loc.getLongitude()));
+
+            final TextView textViewLatitudeValue = (TextView) findViewById(R.id.textViewLatitudeValue);
+            textViewLatitudeValue.setText(String.valueOf(loc.getLatitude()));
+
+            final TextView textViewAccuracyValue = (TextView) findViewById(R.id.textViewAccuracyValue);
+            textViewAccuracyValue.setText(String.valueOf(loc.getAccuracy()) + "m");
+
+            final TextView textViewProviderValue = (TextView) findViewById(R.id.textViewProviderValue);
+            textViewProviderValue.setText(loc.getProvider());
         }
     }
 
     public void registerUser(View view) {
 
-        EditText editText = (EditText) findViewById(R.id.editTextDisplayName);
-        //String value = editText.getText().toString();
+        final EditText editText = (EditText)this.findViewById(R.id.editTextDisplayName);
 
         if (editText.getText() == null || editText.getText().length() < 1) {
             Toast.makeText(this, getString(R.string.ui_setup_noname),
@@ -68,9 +101,51 @@ public class SetupActivity extends Activity {
 
         Location loc = (mainActivity.getActualLocation() != null ? mainActivity.getActualLocation() : null);
 
-        UserRegisterTask userRegisterTask = new UserRegisterTask(this, loc);
-        userRegisterTask.execute();
-        // start Task
-    }
+        final String originalId = getUserID();
 
+        UserUpdateTask userUpdateTask = new UserUpdateTask(originalId, editText.getText().toString(), loc) {
+            @Override
+            public void onPostExecute(UserData result) {
+                if(result != null) {
+                    // We need an Editor object to make preference changes.
+                    // All objects are from android.context.Context
+                    SharedPreferences.Editor editor = settings.edit();
+
+                    editor.putString(PREF_ID, result.getId());
+                    editor.putString(PREF_DISPLAYNAME, result.getDisplayName());
+
+                    // Commit the edits!
+                    editor.commit();
+
+                    finish();
+                }
+
+                final Button button = (Button) findViewById(R.id.button2);
+                button.setVisibility(View.VISIBLE);
+
+                final ProgressBar progressBarSetup = (ProgressBar) findViewById(R.id.progressBarSetup);
+                progressBarSetup.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onPreExecute() {
+                final Button button = (Button) findViewById(R.id.button2);
+                button.setVisibility(View.GONE);
+
+                final ProgressBar progressBarSetup = (ProgressBar) findViewById(R.id.progressBarSetup);
+                progressBarSetup.setVisibility(View.VISIBLE);
+                progressBarSetup.setMax(100);
+                progressBarSetup.setProgress(0);
+            }
+
+            @Override
+            public void onProgressUpdate(Integer... values) {
+                final ProgressBar progressBarSetup = (ProgressBar) findViewById(R.id.progressBarSetup);
+                progressBarSetup.setProgress(values[0]);
+            }
+        };
+
+        // start Task
+        userUpdateTask.execute();
+    }
 }
---
+++ AndroidApp/src/ch/bfh/fbi/mobiComp/PeopleTag/tasks/UserUpdateTask.java
@@ -0,0 +1,135 @@
+package ch.bfh.fbi.mobiComp.PeopleTag.tasks;
+
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.util.Log;
+import ch.bfh.fbi.mobiComp.PeopleTag.gui.SetupActivity;
+import ch.bfh.fbi.mobiComp.PeopleTag.model.UserData;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public abstract class UserUpdateTask extends AsyncTask<String, Integer, UserData> {
+
+    private static final String TAG = "UserUpdateTask"; // for LogCat
+    protected UserData result = null;
+    private Location location;
+    private String userID;
+    private String displayName;
+
+    /*
+     * Constructor
+     */
+    public UserUpdateTask(String userId, String displayName, Location location) {
+        this.userID = userId;
+        this.displayName = displayName;
+        this.location = location;
+    }
+
+    public static HttpResponse makePostRequest(String uri, String json) {
+        try {
+            HttpPost httpPost = new HttpPost(uri);
+            httpPost.setEntity(new StringEntity(json));
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+            return new DefaultHttpClient().execute(httpPost);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            Log.e(TAG, "makePostRequest() -> UnsupportedEncodingException", e);
+        } catch (ClientProtocolException e) {
+            Log.e(TAG, "makePostRequest() -> ClientProtocolException", e);
+        } catch (IOException e) {
+            Log.e(TAG, "makePostRequest() -> IOException", e);
+        }
+        return null;
+    }
+
+    @Override
+    public abstract void onPostExecute(UserData result);
+
+    @Override
+    public abstract void onPreExecute();
+
+    @Override
+    public abstract void onProgressUpdate(Integer... values);
+
+
+    @Override
+    protected UserData doInBackground(String... Params) { // this method runs in dedicated non-UI thread
+
+        Boolean querySuccessful = false;
+
+        String jsonResponse = null;
+
+        String url = (this.userID != null && this.userID.length() > 0 ? "http://peopletag.xrj.ch/users/" + this.userID : "http://peopletag.xrj.ch/users");
+
+
+        String longitude = (location != null ? String.valueOf(location.getLongitude()) : "null");
+        String latitude = (location != null ? String.valueOf(location.getLatitude()) : "null");
+
+        this.publishProgress(20);
+
+
+        String json = "{ " +
+                "\"displayName\": \"" + displayName + "\"," +
+                "\"currentLongitude\": " + longitude + "," +
+                "\"currentLatitude\": " + latitude +
+                " }";
+
+        try {
+            HttpResponse response = makePostRequest(url, json);
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                jsonResponse = EntityUtils.toString(response.getEntity());
+                Log.d(TAG, "POST " + url + " returned Code " + response.getStatusLine().getStatusCode() + " with Content: " + jsonResponse);
+                querySuccessful = true;
+            } else {
+                Log.e(TAG, "POST " + url + " returned Code " + response.getStatusLine().getStatusCode() + " with Error-Content: " + response.getEntity());
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error loading " + url, e);
+        }
+
+        this.publishProgress(50);
+
+        if (jsonResponse == null || !querySuccessful) {
+            return null;
+        }
+
+        try {
+            // retrieve the object that contains the array named "data" --> see structure of the json response
+            JSONObject rootJsonObject = new JSONObject(jsonResponse);
+
+            this.publishProgress(100);
+
+            // the container contains name-value pairs -> retrieve value by name
+            if(rootJsonObject.has("_id")) {
+                // mind security threats resulting from the get method internally using eval
+                // (exploits by malicious content in the response are feasible)
+                // we do not fix this for the moti project as security is not a concern
+                return new UserData(rootJsonObject.get("_id").toString(),
+                        rootJsonObject.get("displayName").toString(),
+                        rootJsonObject.getDouble("currentLatitude"),
+                        rootJsonObject.getDouble("currentLongitude"));
+            }
+            else if (rootJsonObject.has("msg")) {
+                return new UserData(userID, displayName, (location != null ? location.getLongitude() : null), (location != null ? location.getLatitude() : null));
+            }
+
+        } catch (JSONException e) {
+            Log.e(TAG, "JSON Exception", e);
+        }
+        return null;
+    }
+
+}
+
